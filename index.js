da3cc77eb3e4adbed92bd289d1eb90c78f77a003
Md. Noushin Islam <noushinshoha@gmail.com>
2025-02-22 18:44:01 +0600

    new operations added "

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // middleware
 app.use(cors({
     origin: ['http://localhost:5173', 'https://task-manager-bd1a7.web.app'],
     credentials: true,
 }));
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmmth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -24,16 +22,18 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
 async function run() {
     try {
         const userCollection = client.db("taskManagerDb").collection("users");
         const taskCollection = client.db("taskManagerDb").collection("tasks");
+        const activityLogCollection = client.db("taskManagerDb").collection("activityLogs");
 
         // JWT API
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-
             res.send({ token });
         });
 
@@ -71,27 +71,42 @@ async function run() {
             res.send(result);
         });
 
+
         // Get All Tasks
         app.get('/tasks/:email', verifyToken, async (req, res) => {
             const email = req.params.email;
-
             if (email !== req.decoded.email) {
-                return res.status(403).send({ message: "forbidden access" })
+                return res.status(403).send({ message: "Forbidden access" });
             }
-            const filter = { createdBy: email }
-            const tasks = await taskCollection
-                .find(filter)
-                .sort({ status: 1, order: 1 })
-                .toArray();
+            const filter = { createdBy: email };
+            const tasks = await taskCollection.find(filter).sort({ status: 1, order: 1 }).toArray();
             res.send(tasks);
         });
 
         // Update Task
         app.patch('/tasks/:id', verifyToken, async (req, res) => {
-            const result = await taskCollection.updateOne(
-                { _id: new ObjectId(req.params.id) },
-                { $set: req.body }
-            );
+            const taskId = new ObjectId(req.params.id);
+            const updatedData = req.body;
+
+            let message = ''
+
+            const task = await taskCollection.findOne({ _id: taskId });
+
+            if (updatedData.status === undefined || task.status === updatedData.status) {
+                message = `${task.title} is updated `;
+            }
+            else {
+                message = `${task.title} moved to ${updatedData.status.charAt(0).toUpperCase() + updatedData.status.slice(1)}`;
+            }
+
+            await activityLogCollection.insertOne({
+                taskId: task._id,
+                message,
+                createdBy: req.decoded.email,
+                updatedAt: new Date()
+            });
+
+            const result = await taskCollection.updateOne({ _id: taskId }, { $set: updatedData });
             res.send(result);
         });
 
@@ -101,8 +116,18 @@ async function run() {
             res.send(result);
         });
 
-        // await client.db("admin").command({ ping: 1 });
-        console.log("Connected to MongoDB!");
+        // activity logs APIs
+        app.get('/activityLogs/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: "Forbidden access" });
+            }
+            const filter = { createdBy: email };
+            const activityLogs = await activityLogCollection.find(filter).toArray();
+            res.send(activityLogs);
+        })
+
+        // console.log("Connected to MongoDB!");
 
     } catch (error) {
         console.error(" Error connecting to MongoDB:", error);
@@ -112,9 +137,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("Task Manager API with Polling is running");
+    res.send("Task Manager API with Activity Logs is running");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    // console.log(`Server running on port ${port}`);
 });
